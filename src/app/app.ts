c519f9c14ad9ada9189f5114481acd92f25c71bd
David Incarbone <david.incarbone@gmail.com>
2025-08-20 11:38:50 +0200

    refactor: update app component to include necessary Angular pipes and enhance template structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,23 @@
+import {
+  CurrencyPipe,
+  DatePipe,
+  DecimalPipe,
+  LowerCasePipe,
+  PercentPipe,
+  UpperCasePipe,
+} from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe, LowerCasePipe, DatePipe, DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  title = 'ANGULAR-TEST2';
+  // numero = 5.33333333333333;
+  numero = 5;
+  oggi = Date.now();
+}
